c7b2ed865d1b9cbca2270c555711ea6c02207ddc
lateshK <42994943+lateshK@users.noreply.github.com>
2020-06-13 18:41:16 +0530

    new hash

--- goa-salad-bowl-online-order.php
+++ goa-salad-bowl-online-order.php
@@ -565,12 +565,11 @@
 	<script src="https://unpkg.com/react@16.0.0/umd/react.production.min.js"></script>
 	<script src="https://unpkg.com/react-dom@16.0.0/umd/react-dom.production.min.js"></script>	
 	<script src="https://order.greengrainbowl.com/site/firebase-functions.js?_1" type="text/javascript"></script>
-	<script src="https://order.greengrainbowl.com/site/react-components.1592042228323.js" type="text/javascript"></script>
+	<script src="https://order.greengrainbowl.com/site/react-components.1592053842717.js" type="text/javascript"></script>
 	
 	<!-- Global variables for react cart app component and css  hash -->
 	<script>
-		var react_js_file_hashes = 
-{"2":"f5d91756","runtime-main":"9efbcb05","main":"3c4d266b","react-components":"1592042228323"};
+		var react_js_file_hashes = {"2":"c2a15300","runtime-main":"9efbcb05","main":"006aee3e","react-components":"1592053842717"};
 		var react_css_file_hashes = {"main":"6d49d0d5"};
 		var app_url = "https://order.greengrainbowl.com/site";
 		var site_url = "https://greengrainbowl.com";
