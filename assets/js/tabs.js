80be586a969bc5ac298f735b07e8070449e9af7d
Ankita <ankita@ajency.in>
2020-06-14 15:17:26 +0530

    dragging issue fixed

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -77,3 +77,31 @@ jQuery.fn.scrollCenterORI = function(elem, speed) {
   }, speed == undefined ? 1000 : speed);
   return this;
 };
+
+
+const slider = document.querySelector(".tabs");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", e => {
+  isDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+});
+
+slider.addEventListener("mousemove", e => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3;
+  slider.scrollLeft = scrollLeft - walk;
+});
