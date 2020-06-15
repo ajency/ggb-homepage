28c6489ce10630c44807a6c9ad2ece92af1e72d7
Ankita <ankita@ajency.in>
2020-06-15 12:30:13 +0530

    css fix

--- goa-salad-bowl-online-order.php
+++ goa-salad-bowl-online-order.php
@@ -24,6 +24,13 @@
 	<script> 
 		(function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:1856895,hjsv:6}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv='); 
 	</script>
+	<style type="text/css">
+		@media (max-width: 991px) {
+			.add-address-form{
+				padding-bottom: 100px;
+			}
+		}
+	</style>
 </head>
 <body>
 
