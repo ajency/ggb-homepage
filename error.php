27528b5b0f4e189bb7a314708ab08f66608eb39d
Ankita <ankita@ajency.in>
2020-06-16 12:35:05 +0530

    css path changed

--- error.php
+++ error.php
@@ -5,7 +5,7 @@
     <title>Health Food Delivery Goa | Green Grain Bowl | Buy Healthy Food Online</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-	<link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+	<link rel="stylesheet" type="text/css" href="../assets/css/custom.css">
 	<link href="https://fonts.googleapis.com/css?family=Work+Sans:400,500|Libre+Baskerville:400,400i" rel="stylesheet"> 
 	<link crossorigin="anonymous" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" rel="stylesheet">
 	<script src="https://kit.fontawesome.com/4b9ba14b0f.js" crossorigin="anonymous"></script>
@@ -20,7 +20,7 @@
 			<span class="err">4</span>
 		</div>
 		<div class="seperator-img">
-			<img src="assets/images/Leaf_with_seperator@2x.png" class="img-fluid auto-width" alt="green grain bowl" title="green grain bowl">
+			<img src="../assets/images/Leaf_with_seperator@2x.png" class="img-fluid auto-width" alt="green grain bowl" title="green grain bowl">
 		</div>
 		<div class="msg">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first place?
 			<p>Let's go <a href="http://greengrainbowl.com" class="home-link" title="Back To Home">home<span class="link-underline"></span></a> and try from there.</p>
