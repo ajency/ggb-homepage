a2a7e8e8ee54dfb6d1156d5cf473a72a626049a8
nutank <nutan@ajency.in>
2020-07-11 11:11:41 +0530

    Update goa-salad-bowl-online-order.php

--- goa-salad-bowl-online-order.php
+++ goa-salad-bowl-online-order.php
@@ -6788,12 +6788,12 @@
 	<script src="https://unpkg.com/react@16.0.0/umd/react.production.min.js"></script>
 	<script src="https://unpkg.com/react-dom@16.0.0/umd/react-dom.production.min.js"></script>	
 	<script src="https://order.greengrainbowl.com/site/firebase-functions.js?_1" type="text/javascript"></script>
-	<script src="https://order.greengrainbowl.com/site/react-components.1594209966598.js" type="text/javascript"></script>
+	<script src="https://order.greengrainbowl.com/site/react-components.1594445723445.js" type="text/javascript"></script>
 	
 	<!-- Global variables for react cart app component and css  hash -->
 	<script>
-		var react_js_file_hashes = {"2":"4535aed7","runtime-main":"9efbcb05","main":"a4ccb55c","react-components":"1594209966598"}
-		//old{"2":"4535aed7","runtime-main":"9efbcb05","main":"3be0b6f4","react-components":"1593686386024"};
+		var react_js_file_hashes = {"2":"4535aed7","runtime-main":"9efbcb05","main":"a4ccb55c","react-components":"1594445723445"};
+		//old{"2":"4535aed7","runtime-main":"9efbcb05","main":"a4ccb55c","react-components":"1594209966598"}
 		var react_css_file_hashes = {"main":"5ab3505a"};
 		// old 727e096b
 		var app_url = "https://order.greengrainbowl.com/site";
