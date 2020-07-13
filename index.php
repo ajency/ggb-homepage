c5250212ca277f44fdd0f5202e3cf3970abf29f9
latesh <latesh@ajency.in>
2020-07-13 16:30:54 +0530

    dynamic hash and website const

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
-	header("Location: http://greengrainbowl.com/goa-salad-bowl-online-order");
+	require_once('website_constants.php');
+	header("Location:".$APP_URL."/goa-salad-bowl-online-order");
 	exit();
 ?>
