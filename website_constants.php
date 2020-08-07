3bf0505f01b03e4885a72a2913a1adfec5f1ed5c
Ankita <ankita@ajency.in>
2020-08-07 16:33:10 +0530

    updated theme

---
+++ website_constants.php
@@ -0,0 +1,5 @@
+<?php
+$SITE_URL = "http://localhost/ggb-homepage-dev";
+$APP_URL = "http://localhost/greengrainbowl-live/app/build/site";
+$REDIRECT_URL = "http://localhost/ggb-homepage-dev/goa-salad-bowl-online-order.php"
+?>
