532862fb73dc06ecb731682327694eec0f15e320
Viraj Verlekar <viraj@ajency.in>
2020-08-07 18:22:20 +0530

    css path

--- wp-content/themes/ajency-portfolio/goa-salad-bowl-online-order.php
+++ wp-content/themes/ajency-portfolio/goa-salad-bowl-online-order.php
@@ -26,7 +26,7 @@ Template Name: goa-salad-bowl-online-order
     <meta content="Buy healthy food online by Green Grain Bowl. Healthy lunch bowls delivered to you in Panjim/Porvorim, Goa. Enjoy wholesome meals, salad style." name="description" name="twitter:description"/>
     <meta content="Health Food Delivery Goa | Green Grain Bowl | Buy Healthy Food Online" name="twitter:title"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-	<link rel="stylesheet" type="text/css" href="<?php echo get_parent_theme_file_uri( '/assets/css/custom.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="/assets/css/custom.css">
 	<link href="https://fonts.googleapis.com/css?family=Work+Sans:400,500|Libre+Baskerville:400,400i" rel="stylesheet">  
     <link rel="stylesheet" href="https://3ebx62qxxp43hyojr3jaqjv1-wpengine.netdna-ssl.com/wp-content/themes/ajency-portfolio/css/animate.min.css">
     <link rel='icon' type='image/x-icon' href="/assets/images/Favicon-48.png"/>
