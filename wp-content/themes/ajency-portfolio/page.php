64f06125f4f66cde510a6ddd18b8367810bb336f
Ankita <ankita@ajency.in>
2020-08-14 13:25:33 +0530

    page fix

--- wp-content/themes/ajency-portfolio/page.php
+++ wp-content/themes/ajency-portfolio/page.php
@@ -3,7 +3,7 @@
 <section class="post-content page-content">
 	<div class="container mt-5 pdt-3">
 	  	<div class="row">
-		    <div class="">
+		    <div class="w-100">
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
           
 					<?php the_content(); ?>
